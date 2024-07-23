f17a4e47f0abca89b1513ba4b624b636b3b1a202
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-07-23 13:46:11 +0300

    filter function added

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,24 +2,28 @@ import { useEffect, useState } from "react";
 import CardsContainer from "./CardsContainer";
 import LoadMoreButton from "./LoadMoreButton.js";
 import {getData} from "../utils/getData.js";
+import SearchContainer from "./SearchContainer.js";
 
 Main = () => {
   
   const [restaurants, setRestaurants] = useState([]);
   const [offset, setOffset] = useState(1);
-  
+  const [filtered,setFiltered] = useState(restaurants);
   
   useEffect(() => {
     async function handleData(){
       try {
+
         const newRestaurants = await  getData(offset);
-        //console.log(newRestaurants)
+        console.log(newRestaurants)
         setRestaurants([...restaurants,...newRestaurants]);
+        setFiltered([...restaurants,...newRestaurants]);
+        
         
       } catch (error) {
-        console.log(error.message)
+        console.log("Main.js=>catch: " + error.message);
         handleData();
-        //throw new Error("from definition of handle data")
+        
       }
       
     }
@@ -35,19 +39,22 @@ Main = () => {
     
   }, [offset]);
   
-  console.log("before render")
+  
   if(restaurants.length===0){
-    console.log('empty')
+    
     return (
       <div>
         <h1>Shirm</h1>
       </div>
     )
   }else{
-    console.log('with data')
+    
     return (
       <div className="main">
-        <CardsContainer restaurants={restaurants} />
+        <SearchContainer 
+        restaurants={restaurants} 
+        setFiltered={setFiltered}/>
+        <CardsContainer restaurants={filtered} />
         <LoadMoreButton offset={offset} setOffset={setOffset}/>
       </div>
     );
---
+++ src/components/SearchContainer.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+
+
+const SearchContainer=(props)=>{
+    const {restaurants,setFiltered}=props;
+    const [searchText,setSearchText] = useState("");
+    
+
+
+    return (
+        <div className={'search-container'}>
+          <input type="text" onChange={(e)=>{
+            setSearchText(e.target.value);
+          }} value={searchText}></input>
+          <button className="search-btn" onClick={()=>{
+            console.log('handleClick');
+            setFiltered(restaurants.filter(x=>x.info.name.includes(searchText)));
+            
+          }}>Search</button>
+        </div>
+    )
+}
+
+export default SearchContainer;
